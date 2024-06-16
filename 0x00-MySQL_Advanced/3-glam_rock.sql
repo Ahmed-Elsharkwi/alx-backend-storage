359efcee8b8e75c758de859bd4554c81b04bdf44
Ahmed-Elsharkwi <ahmedelsharkwiti@gmail.com>
2024-06-16 19:13:40 +0300

    MM

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,7 +1,7 @@
 -- script which lists all bands with glam rock
 select band_name, 
   case 
-  When split is NULL then 2022 - formed - 2
+  When split is NULL then 2022 - formed
   Else split - formed
   end as lifespan
   from metal_bands
