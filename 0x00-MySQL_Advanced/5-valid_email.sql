bcda8ff6c40fd3819040487265a812e7587f1432
Ahmed-Elsharkwi <ahmedelsharkwiti@gmail.com>
2024-06-18 00:44:04 +0300

    MM

---
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -0,0 +1,9 @@
+delimiter $$
+CREATE TRIGGER change_email BEFORE UPDATE ON users
+FOR EACH ROW
+BEGIN
+    IF NEW.email <> OLD.email THEN
+        SET NEW.valid_email = 0;
+    END IF;
+END $$
+DELIMITER ;
