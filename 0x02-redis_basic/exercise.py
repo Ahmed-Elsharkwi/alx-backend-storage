7fa263b60051553bb0b7394528bd87a8fa7453ca
Ahmed-Elsharkwi <ahmedelsharkwiti@gmail.com>
2024-06-19 20:41:24 +0300

    MM

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -17,8 +17,8 @@ class Cache:
         named _redis (using redis.Redis()) and
         flush the instance using flushdb
         """
-        self.__redis = redis.Redis()
-        self.__redis.flushdb()
+        self._redis = redis.Redis()
+        self._redis.flushdb()
 
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """
@@ -27,5 +27,5 @@ class Cache:
         store the input data in Redis using the random key and return the key.
         """
         random_key = str(uuid.uuid4())
-        self.__redis.set(random_key, data)
+        self._redis.set(random_key, data)
         return random_key
